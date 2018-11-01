0889a03bb269983c7048cb45a090f11c50ee9d1b
github username <github email address>
2018-11-01 10:07:18 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'about', to: "static#about"
+  get 'posts/:id', to: 'posts#show'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
